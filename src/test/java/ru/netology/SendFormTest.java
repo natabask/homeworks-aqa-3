9dbb66eba8573b2e505879ee9815feac2cdebe1c
natabask <natabask@yandex.ru>
2022-05-08 21:00:11 +0300

    test changed with selenide - positive test should pass

--- src/test/java/ru/netology/SendFormTest.java
+++ src/test/java/ru/netology/SendFormTest.java
@@ -1,35 +1,12 @@
 package ru.netology;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SendFormTest {
-
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUp() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
-    }
-
-    @BeforeEach
-    void setUp2() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    public void close() {
-        driver.quit();
-        driver = null;
-    }
 
     @Test
     public void shouldPassWhenAllDataCorrect() {
